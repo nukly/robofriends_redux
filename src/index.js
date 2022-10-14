b07d5d2341e0160fc0108ed9d55630b7eb9a0db3
nukly <klemen.brecko@gmail.com>
2022-10-14 09:14:57 +0200

    Initial
    Initial commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import './index.css';
 import App from './containers/App';
 import 'tachyons';
 // import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { createStore }from 'redux'
+import { searchRobots } from './reducers';
 
-
+const store = createStore(searchRobots)
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /></Provider>);
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
